00d6b2f073ac8a25f088affe5c94ed80acc28d11
qiushifa <shifa.qiu@choiceform.com>
2021-10-28 16:52:54 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC<{}> = () => (
+  <div>
+    <header>
+      <p>
+        dfsddfa
+      </p>
+    </header>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // 通过 npm 安装
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    // Add ".ts" and ".tsx" as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+
+};
